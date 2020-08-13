456394abef527b19b570512dced9d95745d68630
Belyaeva Ekaterina <katrinamalinashtorm@gmail.com>
2020-08-13 18:04:44 +0600

    Добавлен компонент Joke.js и стили Joke.css

---
+++ src/components/Joke/Joke.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Joke.css';
+
+const Joke = props => {
+        return (
+            <div className="Joke">
+                    <div className="joke">{props.text}</div>
+            </div>
+        );
+};
+
+export default Joke;
